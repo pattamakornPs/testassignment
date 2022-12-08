4efbba2ae7e9dd37a7e5ecefb8a657e34aff1104
pattamakornPs <44288370+pattamakornPs@users.noreply.github.com>
2022-12-08 18:32:32 +0700

    comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import App from "./page/home/index";
 import DetailApp from './page/detailrestaurant/index'
 import reportWebVitals from './reportWebVitals';
@@ -9,8 +7,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
-  useParams
 } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
@@ -23,12 +19,6 @@ root.render(
       <Route path="/:id/:name" element={<DetailApp />} />
     </Routes>
   </Router>
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
